65212375898630e14d936fd44d425d11171651a7
3bop <hh913843@gmail.com>
2025-12-14 23:25:18 +0200

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,31 @@
+#-------------
+#-- Numbers --
+#-------------
+ 
+# Integer
+
+print(type(1))
+print(type(10))
+print(type(100))
+print(type(-10))
+
+# Float
+
+print(type(1.500))
+print(type(100.99))
+print(type(-10.99))
+print(type(0.99))
+print(type(-0.99))
+
+ # Complex
+
+mycomplexnumber = 5+6j
+
+print(type(mycomplexnumber))
+
+print("Real Part is:{}".format(mycomplexnumber))
+
+#[1] You can convert from int to float or coplex
+#[2] You can convert from float to int or coplex
+#[3] You connot convert complex to any type
+ 
